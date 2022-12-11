ae434875e6892d25e57235584b98339df188e2dc
Darío Aboy <darii0_-@hotmail.com>
2022-12-11 23:11:40 +0100

    Add ability to vote jokes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Button, Container, Heading, Text, useBreakpointValue, VStack } from '@chakra-ui/react';
+import { Button, Container, Heading, HStack, Text, Toast, useBreakpointValue, VStack } from '@chakra-ui/react';
+import { FaThumbsUp, FaThumbsDown } from 'react-icons/fa';
+import toast, { Toaster } from 'react-hot-toast';
 
 import './home.css';
 
@@ -8,7 +10,9 @@ interface Joke {
     id: number,
     type: string,
     setup: string,
-    punchline: string
+    punchline: string,
+    likes: number,
+    dislikes: number
 }
 
 const Home = () => {
@@ -28,7 +32,20 @@ const Home = () => {
         setPunchlineShowing(!punchlineShowing);
     }
 
+    const sendVote = (joke_id: number, type: string) => axios.post('http://127.0.0.1:3000/api/joke/vote', {joke_id, type}).then(response => setCurrentJoke(response.data));
+
+    const sendLike = () => {
+        toast('You liked this joke!', {position: 'bottom-center'});
+        sendVote(currentJoke?.id as number, 'like');
+    }
+
+    const sendDislike = () => {
+        toast('You giva a dislike to this joke :(', {position: 'bottom-center'});
+        sendVote(currentJoke?.id as number, 'dislike');
+    }
+
     return <Container maxW='container.sm'>
+        <Toaster />
         <VStack>
             <Heading fontWeight={600} fontSize={{base: '3xl', sm: '4xl', md: '6xl'}}>
                 random<Text as='span' color='cyan.800'>Jokes</Text>
@@ -39,9 +56,17 @@ const Home = () => {
         </Heading>
         {punchlineShowing && <Text fontSize='2xl' marginTop='1rem'>{currentJoke?.punchline}</Text>}
         <VStack marginTop='3rem'>
-            <Button colorScheme={'cyan'} color='white' bg={'cyan.800'} rounded={'full'} px={12} _hover={{bg: 'cyan.900'}} onClick={buttonPress}>
-                {!punchlineShowing ? 'Reveal punchline' : 'Next joke'}
-            </Button>
+            <Container display='flex' justifyContent='center'>
+                {punchlineShowing && <Button colorScheme='cyan' color='white' bg='cyan.800' rounded='full' px='6' _hover={{bg: 'cyan.900'}} marginRight='15px' onClick={sendLike}>
+                    {currentJoke?.likes}&nbsp;&nbsp;<FaThumbsUp />
+                </Button>}
+                <Button colorScheme={'cyan'} color='white' bg={'cyan.800'} rounded={'full'} px={12} _hover={{bg: 'cyan.900'}} onClick={buttonPress}>
+                    {!punchlineShowing ? 'Reveal punchline' : 'Next joke'}
+                </Button>
+                {punchlineShowing && <Button colorScheme='cyan' color='white' bg='cyan.800' rounded='full' px='6' _hover={{bg: 'cyan.900'}} marginLeft='15px' onClick={sendDislike}>
+                    {currentJoke?.dislikes}&nbsp;&nbsp;<FaThumbsDown />
+                </Button>}
+            </Container>
         </VStack>
     </Container>
 }
